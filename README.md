7cbdadf7afd9a610d3287d03cf60c0b90079e672
Travis <travislawclimb@gmail.com>
2021-01-28 12:23:22 -0800

    Repos ready

---
+++ README.md
@@ -0,0 +1,2 @@
+# Steezn
+It's Steezin season and this application will hook you up 10/10 with a monthly subscription custom put together outfits sent to your home.
---
+++ Steezn/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Steezn.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
